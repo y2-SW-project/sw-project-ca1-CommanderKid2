f7057f42e3108c0ec8d154bbeb02aff072aedaf8
CommanderKid2 <91547715+CommanderKid2@users.noreply.github.com>
2022-02-24 10:51:10 +0000

    add blades

--- Laravel/SWProjectDarwin/routes/web.php
+++ Laravel/SWProjectDarwin/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/login', [LoginController::class, 'login']);
+
+Route::get('/login', [LoginController::class, 'login']);
+
+Route::get('about', 'App\Http\Controllers\AboutController@about');
+
+Route::get('register', function (){
+    return view('register');
 });

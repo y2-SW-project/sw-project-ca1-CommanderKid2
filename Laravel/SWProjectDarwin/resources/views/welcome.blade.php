c2410cb632cf968ad406a62d9edcbf89db3420ec
CommanderKid2 <91547715+CommanderKid2@users.noreply.github.com>
2022-03-16 09:36:02 +0000

    add bootstrap

--- Laravel/SWProjectDarwin/resources/views/welcome.blade.php
+++ Laravel/SWProjectDarwin/resources/views/welcome.blade.php
@@ -7,6 +7,7 @@
         <title>Laravel</title>
 
         <!-- Fonts -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-uWxY/CJNBR+1zjPWmfnSnVxwRheevXITnMqoEIeG1LJrdI0GlVs/9cVSyPYXdcSF" crossorigin="anonymous">
         <link rel="stylesheet" href="/css/app.css">
         <link rel="stylesheet" href="{{ asset('css/app.css') }}" type="text/css">
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -24,11 +25,11 @@
 
 
     <body>
-        <a href="/login"> Login</a>
+        {{-- <a href="/login"> Login</a>
         <a href="/register"> Register</a>
-        <a href="/register"> About</a>
+        <a href="/register"> About</a> --}}
 
-        <nav class="navbar navbar-light bg-light">
+        {{-- <nav class="navbar navbar-light bg-light">
 
               <a class="navbar-brand" href="#">Navbar</a>
 
@@ -47,6 +48,59 @@
                 </ul>
 
             </div>
+          </nav> --}}
+          <nav class="navbar navbar-expand-lg navbar-dark bg-warning shadow-lg">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="#">AceFly</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Categories</a>
+                  </li>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Learn More</a>
+                </li>
+                  <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                      More
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                      <li><a class="dropdown-item" href="#">Action</a></li>
+                      <li><a class="dropdown-item" href="#">Another action</a></li>
+                      <li><hr class="dropdown-divider"></li>
+                      <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                  </li>
+                </ul>
+                <li class="d-flex">
+                  <button type="button" class="btn btn-light rounded-pill"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
+                  </svg> Purchase now</button>
+                </li>
+              </div>
+            </div>
           </nav>
+
+
+          <div class="container ">
+            <div class="row align-items-center my-5">
+                <div class="col-md-5"><img src="/imgs/1.svg" class="img-fluid mt-3 " alt="one people looking at a laptop"></div>
+                <div class="col-md-6 text-center text-md-start">
+                  <p class="display-4">Change the way you blind <br>websites. Forever.</p>
+                  <p class="display-6 text-secondary">With AceFly you get components and examples, <br>including tons of variables that will help you <br> customizet this drone.</p>
+                  <br>
+                  <h3><span class="text-warning">Purschase now</span></h3>
+              </div>
+              </div>
+          </div>
+
+          <body>
     </body>
 </html>
